ea180e721b7ebf5701d18bb63b4a44001dc57435
Rodrigo <digo_gabriel97@yahoo.com.br>
2023-10-29 21:10:18 -0300

    event-emitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IfRenderComponent } from './compenent/if-render/if-render.component';
 import { EventosComponent } from './compenent/eventos/eventos.component';
+import { EmitterComponent } from './compenent/emitter/emitter.component';
+import { ChangeNumberComponent } from './compenent/change-number/change-number.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IfRenderComponent,
-    EventosComponent
+    EventosComponent,
+    EmitterComponent,
+    ChangeNumberComponent
   ],
   imports: [
     BrowserModule
